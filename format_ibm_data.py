aed6579f8fbfe24a332c05df17ac4622ce2c616b
Wei Lee <weilee.rx@gmail.com>
2019-12-31 18:30:56 +0800

    style(all): blackify

--- format_ibm_data.py
+++ format_ibm_data.py
@@ -1,6 +1,10 @@
 from itertools import islice, product
 
-from Association_Rule import find_frequent_patterns, generate_association_rules, load_data
+from Association_Rule import (
+    find_frequent_patterns,
+    generate_association_rules,
+    load_data,
+)
 
 
 def rolling_window(seq, n=2):
@@ -15,8 +19,8 @@ def rolling_window(seq, n=2):
 
 
 def transactions_to_graph(transactions, output_filename):
-    EDGE_FORMAT_STR = '{source},{dest}\n'
-    with open(output_filename, 'w') as output_file:
+    EDGE_FORMAT_STR = "{source},{dest}\n"
+    with open(output_filename, "w") as output_file:
         for transaction in transactions:
             for edge in rolling_window(transaction):
                 source, dest = edge
@@ -24,24 +28,24 @@ def transactions_to_graph(transactions, output_filename):
 
 
 def association_rule_to_graph(association_rules, output_filename):
-    EDGE_FORMAT_STR = '{source},{dest}\n'
+    EDGE_FORMAT_STR = "{source},{dest}\n"
 
     edges = set()
     for cause, effect in association_rules.keys():
         for p in product(cause, effect):
             edges.add(p)
 
-    with open(output_filename, 'w') as output_file:
+    with open(output_filename, "w") as output_file:
         for source, dest in edges:
             output_file.write(EDGE_FORMAT_STR.format(source=source, dest=dest))
 
 
 if __name__ == "__main__":
-    input_filename = './ibm_data/data.ntrans_5.tlen_3.nitems_0.01'
+    input_filename = "./ibm_data/data.ntrans_5.tlen_3.nitems_0.01"
     min_support = 0.2
     min_confident = 0.4
-    output_file1 = 'dataset/graph_7.txt'
-    output_file2 = 'dataset/graph_8.txt'
+    output_file1 = "dataset/graph_7.txt"
+    output_file2 = "dataset/graph_8.txt"
 
     transactions = list(load_data(input_filename))
     transactions_to_graph(transactions, output_file1)
